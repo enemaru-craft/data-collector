528fd75e53fd712b65191d530ba9c08c7f117069
GoRuGoo <dachanyr8989@gmail.com>
2025-08-10 12:47:35 +0900

    refactor: 変数名にdescriptionを追加

--- stg-infrastructure/modules/lambda/output.tf
+++ stg-infrastructure/modules/lambda/output.tf
@@ -1,9 +1,11 @@
 # IoT Coreデータ加工用のLambda関数のARNと名前を出力する
 
 output "lambda_arn" {
-  value = aws_lambda_function.mqtt_data_controller.arn
+  value       = aws_lambda_function.mqtt_data_controller.arn
+  description = "IoT Coreのデータ加工用LambdaのARN"
 }
 
 output "function_name" {
-  value = aws_lambda_function.mqtt_data_controller.function_name
+  value       = aws_lambda_function.mqtt_data_controller.function_name
+  description = "IoT Coreのデータ加工用Lambdaの関数名"
 }

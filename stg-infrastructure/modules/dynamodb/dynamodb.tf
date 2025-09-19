be650f8c910fc97bcb59932c54c1eff2b11f5829
GoRuGoo <dachanyr8989@gmail.com>
2025-09-20 05:31:32 +0900

    feat: PK,SKを修正

--- stg-infrastructure/modules/dynamodb/dynamodb.tf
+++ stg-infrastructure/modules/dynamodb/dynamodb.tf
@@ -2,8 +2,13 @@ resource "aws_dynamodb_table" "tmp_table" {
   name         = "tmp_table"
   billing_mode = "PAY_PER_REQUEST"
 
-  hash_key = "device_id"
+  hash_key  = "session_id"
+  range_key = "device_id"
 
+  attribute {
+    name = "session_id"
+    type = "S"
+  }
   attribute {
     name = "device_id"
     type = "S"
